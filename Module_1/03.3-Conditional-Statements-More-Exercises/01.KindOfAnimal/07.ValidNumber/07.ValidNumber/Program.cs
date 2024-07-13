6b149ef59a1d1b18a4aaac26dfa372cfeb718005
GabrielPilichev <gabriel.pilichev.bg@gmail.com>
2024-07-13 18:09:30 +0300

    Add files via upload

---
+++ Module_1/03.3-Conditional-Statements-More-Exercises/01.KindOfAnimal/07.ValidNumber/07.ValidNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace _07.ValidNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write a program that checks whether a number entered by the user is valid.
+            For a number to be valid, it must be greater than or equal to 1 and at the same time 
+            divisible by 5 without a remainder.If the number is valid, print "Valid!".Otherwise, print "Invalid!".*/
+
+            // Prompt the user to enter a number
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            // Check if the number is valid
+            if (number >= 1 && number % 5 == 0)
+            {
+                Console.WriteLine("Valid!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid!");
+            }
+        }
+    }
+}
