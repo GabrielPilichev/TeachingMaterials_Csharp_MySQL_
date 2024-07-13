82730e81689566f6eb10eef3dab2c8a86b207ea4
GabrielPilichev <gabriel.pilichev.bg@gmail.com>
2024-07-13 19:15:14 +0300

    Add files via upload

---
+++ Module_1/04.1-Loops-Basics-Exercises/01.NumbersFrom1To100/01.NumbersFrom1To100/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.NumbersFrom1To100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that prints the numbers 1 to 100, each on a new line.
+
+            // Loop through numbers 1 to 100
+            for (int i = 1; i <= 100; i++)
+            {
+                // Print each number on a new line
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
