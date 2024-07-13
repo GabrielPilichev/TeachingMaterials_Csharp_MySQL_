85100a23b8ed85a19a30e77be7344a4765043455
GabrielPilichev <gabriel.pilichev.bg@gmail.com>
2024-07-13 17:55:23 +0300

    Add files via upload

---
+++ Module_1/03.3-Conditional-Statements-More-Exercises/01.KindOfAnimal/01.KindOfAnimal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Xml.Linq;
+
+namespace _01.KindOfAnimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write a program that prints the species of the animal according to its name entered by the user.
+                1.dog->mammal
+                2.crocodile, tortoise, snake->reptile
+                3.others->unknown*/
+
+            // Prompt the user to enter the animal name
+            Console.Write("Enter the animal name: ");
+            string animalName = Console.ReadLine().ToLower();
+
+            // Determine the species based on the animal name
+            string species = "";
+
+            switch (animalName)
+            {
+                case "dog":
+                    species = "mammal";
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    species = "reptile";
+                    break;
+                default:
+                    species = "unknown";
+                    break;
+            }
+
+            // Output the species
+            Console.WriteLine($"The species of {animalName} is {species}");
+        }
+    }
+}
