668a5c69ddbe700b50b9e8c14e051ad07eb30320
GabrielPilichev <gabriel.pilichev.bg@gmail.com>
2024-07-13 19:37:10 +0300

    Add files via upload

---
+++ Module_1/04.1-Loops-Basics-Exercises/12.SmallestNumber/12.SmallestNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
+
+namespace _12.SmallestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write a program that, until receiving the "Stop" command, reads integers entered by the user
+            and finds the smallest among them. Enter one number per line.*/
+
+            int minNumber = int.MaxValue;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "stop")
+                    break;
+
+                if (int.TryParse(input, out int number))
+                {
+                    if (number < minNumber)
+                    {
+                        minNumber = number;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer or 'Stop' to finish.");
+                }
+            }
+
+            Console.WriteLine($"The smallest number entered is: {minNumber}");
+        }
+    }
+}
