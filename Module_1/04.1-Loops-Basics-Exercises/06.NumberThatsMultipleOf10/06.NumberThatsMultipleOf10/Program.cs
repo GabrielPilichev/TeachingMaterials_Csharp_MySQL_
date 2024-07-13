54bb826398ae1f55c520265cc608bc3b24a0dc96
GabrielPilichev <gabriel.pilichev.bg@gmail.com>
2024-07-13 19:24:02 +0300

    Add files via upload

---
+++ Module_1/04.1-Loops-Basics-Exercises/06.NumberThatsMultipleOf10/06.NumberThatsMultipleOf10/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _06.NumberThatsMultipleOf10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads an integer n entered by the user.
+            // When entering a number that is not divisible by 10 without a remainder,
+            // print an error message and prompt the user to enter a new number until an appropriate number is entered.
+
+            int n;
+
+            while (true)
+            {
+                Console.Write("Enter a number divisible by 10: ");
+                n = int.Parse(Console.ReadLine());
+
+                if (n % 10 == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Error: The number is not divisible by 10. Please try again.");
+            }
+
+            Console.WriteLine("You entered a valid number: " + n);
+        }
+    }
+}
