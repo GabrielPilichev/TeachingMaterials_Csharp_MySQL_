4c26d0d19eb694e77999dff09e23f9721247799a
GabrielPilichev <gabriel.pilichev.bg@gmail.com>
2024-07-14 16:01:19 +0300

    Add files via upload

--- Module_1/04.3-Loops-More-Exercises/01.NumbersFrom1000EndingIn7/01.NumbersFrom1000EndingIn7/Program.cs
+++ Module_1/04.3-Loops-More-Exercises/01.NumbersFrom1000EndingIn7/01.NumbersFrom1000EndingIn7/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace _01.NumbersFrom1000EndingIn7
+﻿using System.ComponentModel;
+
+namespace _01.NumbersFrom1000EndingIn7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Write a program that prints the numbers in the range[1…1000] that end in 7.*/
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
---
+++ Module_1/04.3-Loops-More-Exercises/02.SumOfPairs/02.SumOfPairs/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.SumOfPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a pair of numbers and calculates the sum of each. 
+            // If the sum of a pair is greater than a given number n, print "Bigger Sum!" on the console.
+            // The console reads:
+            // • n – an integer to which the sum will be compared
+            // • m – an integer in the range [1..100], which represents the number of pairs of numbers 
+            //   that will be entered on the following lines
+            // • On the next m-rows – two integers on separate rows (one pair)
+
+            int n = int.Parse(Console.ReadLine()); // the number to compare sums against
+            int m = int.Parse(Console.ReadLine()); // number of pairs of numbers
+
+            bool foundBiggerSum = false;
+
+            for (int i = 0; i < m; i++)
+            {
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+
+                int sum = num1 + num2;
+
+                if (sum > n)
+                {
+                    foundBiggerSum = true;
+                }
+            }
+
+            if (foundBiggerSum)
+            {
+                Console.WriteLine("Bigger Sum!");
+            }
+        }
+    }
+}
