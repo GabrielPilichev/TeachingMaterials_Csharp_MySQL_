3b98044e886c1087ca0cb6e2d32c713c886b96d3
GabrielPilichev <gabriel.pilichev.bg@gmail.com>
2024-07-14 16:03:08 +0300

    Add files via upload

---
+++ Module_1/04.3-Loops-More-Exercises/03.SumWords/03.SumWords/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03.SumWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that, at the command "Go", collects three words and prints them as one. 
+            // With the "End" command, the program ends.
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "End")
+                {
+                    break;
+                }
+                else if (command == "Go")
+                {
+                    string word1 = Console.ReadLine();
+                    string word2 = Console.ReadLine();
+                    string word3 = Console.ReadLine();
+
+                    string combinedWords = word1 + word2 + word3;
+                    Console.WriteLine(combinedWords);
+                }
+            }
+        }
+    }
+}
